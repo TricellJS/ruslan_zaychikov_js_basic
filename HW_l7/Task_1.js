e580198d6d36d43b50b104101b03eb3a52f36f99
TricellJS <163538526+TricellJS@users.noreply.github.com>
2024-05-27 17:00:34 +0300

    Delete HW_l7 directory

--- HW_l7/Task_1.js
+++
@@ -1,7 +0,0 @@
-function reverseArray(arr) {
-    return arr.reverse();
-}
-
-const originalArray = [1, 2, 3, 4, 5];
-const reversedArray = reverseArray(originalArray);
-console.log(reversedArray); // Виведе: [5, 4, 3, 2, 1]
